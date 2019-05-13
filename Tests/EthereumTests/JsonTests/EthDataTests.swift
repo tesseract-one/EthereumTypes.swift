646f34877aad0ff0ede8d04b3c92022a558a48e1
Yura <yura.kulinich.ua@gmail.com>
2019-05-13 17:06:41 +0300

    Added EthData tests

---
+++ Tests/EthereumTests/JsonTests/EthDataTests.swift
@@ -0,0 +1,55 @@
+//
+//  EthereumDataTests.swift
+//  Web3_Tests
+//
+//  Created by Koray Koska on 13.02.18.
+//  Copyright © 2018 Boilertalk. All rights reserved.
+//
+
+import XCTest
+
+@testable import Ethereum
+
+class EthDataTests: XCTestCase {
+    
+    func testInitialization() {
+        let data1 = EthData(Data([0xab, 0xcf, 0x45, 0x01]))
+        XCTAssertEqual(data1.data, Data([0xab, 0xcf, 0x45, 0x01]), "should initialize correctly")
+
+        let data2 = EthData(Data([0xab, 0xcf, 0x45, 0x01]))
+        XCTAssertEqual(data1, data2, "should be equatable")
+        
+        XCTAssertEqual(data1.hashValue, data2.hashValue, "should produce correct hashValues")
+    }
+    
+    func testEthValueConvertible() {
+        let data1 = try? EthData(ethereumValue: "0x01020304ff")
+        XCTAssertNotNil(data1, "should initialize correctly")
+        XCTAssertEqual(data1?.data, Data([0x01, 0x02, 0x03, 0x04, 0xff]), "should initialize correctly")
+        XCTAssertEqual(data1?.hex, "0x01020304ff", "should initialize correctly")
+        
+        let data2 = try? EthData(ethereumValue: "0x")
+        XCTAssertNotNil(data2)
+        XCTAssertEqual(data2?.data, Data([]), "should initialize correctly")
+        XCTAssertEqual(data2?.hex, "0x", "should initialize correctly")
+        
+        XCTAssertThrowsError(try EthData(ethereumValue: true)) { err in
+            XCTAssertEqual(err as! ValueInitializableError, ValueInitializableError.notInitializable, "should be invalid initialization")
+        }
+        XCTAssertThrowsError(try EthData(ethereumValue: 123)) { err in
+            XCTAssertEqual(err as! ValueInitializableError, ValueInitializableError.notInitializable, "should be invalid initialization")
+        }
+        XCTAssertThrowsError(try EthData(ethereumValue: [true, false])) { err in
+            XCTAssertEqual(err as! ValueInitializableError, ValueInitializableError.notInitializable, "should be invalid initialization")
+        }
+        XCTAssertThrowsError(try EthData(ethereumValue: "//()...")) { err in
+            print(err)
+            XCTAssertEqual(err as! EthData.Error, EthData.Error.hexIsMalformed, "should be invalid initialization")
+        }
+        
+        let data3 = try? EthData.string("0xabffcc").data
+        XCTAssertNotNil(data3, "should return correct data")
+        XCTAssertEqual(data3?.trimmedHex, "0xabffcc", "should return correct data")
+        XCTAssertEqual(data3?.data, Data([0xab, 0xff, 0xcc]), "should return correct data")
+    }
+}
